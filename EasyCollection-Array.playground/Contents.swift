f29d62e1228aa1a240bd83830a69ae959975c4eb
ancheng <cheng.an@duodian.com>
2019-02-13 18:34:00 +0800

    new

--- EasyCollection-Array.playground/Contents.swift
+++ EasyCollection-Array.playground/Contents.swift
@@ -194,4 +194,37 @@ class Solution {
         }
         return result
     }
+
+    /*
+     加一
+     给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     你可以假设除了整数 0 之外，这个整数不会以零开头。
+     示例 1:
+     输入: [1,2,3]
+     输出: [1,2,4]
+     解释: 输入数组表示数字 123。
+     示例 2:
+     输入: [4,3,2,1]
+     输出: [4,3,2,2]
+     解释: 输入数组表示数字 4321。
+    */
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var index = digits.count - 1
+        var res = digits
+        while index >= 0 {
+            if digits[index] != 9 {
+                res[index] = digits[index] + 1
+                return res
+            } else {
+                res[index] = 0
+                if index == 0, digits[index] == 9 {
+                    res.insert(1, at: 0)
+                    return res
+                }
+                index -= 1
+            }
+        }
+        return digits
+    }
 }
