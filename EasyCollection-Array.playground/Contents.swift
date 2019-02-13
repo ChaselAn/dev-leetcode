fa2a84fc5ec036fadf41cda40feec2abdd6f34a3
ancheng <cheng.an@duodian.com>
2019-02-13 19:30:27 +0800

    new

--- EasyCollection-Array.playground/Contents.swift
+++ EasyCollection-Array.playground/Contents.swift
@@ -227,4 +227,32 @@ class Solution {
         }
         return digits
     }
+
+    /*
+     移动零
+     给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     示例:
+     输入: [0,1,0,3,12]
+     输出: [1,3,12,0,0]
+     说明:
+     必须在原数组上操作，不能拷贝额外的数组。
+     尽量减少操作次数。
+    */
+    func moveZeroes(_ nums: inout [Int]) {
+        var lastZeroIndex: Int?
+        var extraZero = 0
+        for (i, num) in nums.enumerated() {
+            if num == 0 {
+                if lastZeroIndex == nil {
+                    lastZeroIndex = i
+                } else {
+                    extraZero += 1
+                }
+            } else if let tempLastZeroIndex = lastZeroIndex {
+                nums[tempLastZeroIndex] = nums[i]
+                nums[i] = 0
+                lastZeroIndex = i - extraZero
+            }
+        }
+    }
 }
