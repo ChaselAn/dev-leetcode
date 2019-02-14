6ac880c6655e6c5d0a2f32b1e9ed961eedf8f794
ancheng <cheng.an@duodian.com>
2019-02-14 16:51:45 +0800

    new

--- EasyCollection-Array.playground/Contents.swift
+++ EasyCollection-Array.playground/Contents.swift
@@ -274,4 +274,102 @@ class Solution {
         }
         return []
     }
+
+    /*
+     有效的数独
+     判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+     1.数字 1-9 在每一行只能出现一次。
+     2.数字 1-9 在每一列只能出现一次。
+     3.数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+     数独部分空格内已填入了数字，空白格用 '.' 表示。
+     示例 1:
+     输入:
+     [
+     ["5","3",".",".","7",".",".",".","."],
+     ["6",".",".","1","9","5",".",".","."],
+     [".","9","8",".",".",".",".","6","."],
+     ["8",".",".",".","6",".",".",".","3"],
+     ["4",".",".","8",".","3",".",".","1"],
+     ["7",".",".",".","2",".",".",".","6"],
+     [".","6",".",".",".",".","2","8","."],
+     [".",".",".","4","1","9",".",".","5"],
+     [".",".",".",".","8",".",".","7","9"]
+     ]
+     输出: true
+     示例 2:
+     输入:
+     [
+     ["8","3",".",".","7",".",".",".","."],
+     ["6",".",".","1","9","5",".",".","."],
+     [".","9","8",".",".",".",".","6","."],
+     ["8",".",".",".","6",".",".",".","3"],
+     ["4",".",".","8",".","3",".",".","1"],
+     ["7",".",".",".","2",".",".",".","6"],
+     [".","6",".",".",".",".","2","8","."],
+     [".",".",".","4","1","9",".",".","5"],
+     [".",".",".",".","8",".",".","7","9"]
+     ]
+     输出: false
+     解释: 除了第一行的第一个数字从 5 改为 8 以外，空格内其他数字均与 示例1 相同。
+     但由于位于左上角的 3x3 宫内有两个 8 存在, 因此这个数独是无效的。
+     说明:
+     一个有效的数独（部分已被填充）不一定是可解的。
+     只需要根据以上规则，验证已经填入的数字是否有效即可。
+     给定数独序列只包含数字 1-9 和字符 '.' 。
+     给定数独永远是 9x9 形式的。
+    */
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        func isSingleNum(_ nums: [Character]) -> Bool {
+            var set = Set<Character>()
+            for num in nums where num != "." {
+                if !set.contains(num) {
+                    set.insert(num)
+                } else {
+                    return false
+                }
+            }
+            return true
+        }
+
+        var columnBoard: [[Character]] = []
+        var nineBoard: [[Character]] = []
+        for (rowIndex, row) in board.enumerated() {
+            if !isSingleNum(row) {
+                return false
+            }
+            if rowIndex == 0 {
+                for char in row {
+                    columnBoard.append([char])
+                }
+            } else {
+                for (index, char) in row.enumerated() {
+                    columnBoard[index].append(char)
+                }
+            }
+            if rowIndex % 3 == 0 {
+                for (index, char) in row.enumerated() {
+                    if index % 3 == 0 {
+                        nineBoard.append([char])
+                    } else {
+                        nineBoard[(index - 1) / 3 + (rowIndex / 3) * 3].append(char)
+                    }
+                }
+            } else {
+                for (index, char) in row.enumerated() {
+                    nineBoard[index / 3 + (rowIndex / 3) * 3].append(char)
+                }
+            }
+        }
+        for column in columnBoard {
+            if !isSingleNum(column) {
+                return false
+            }
+        }
+        for nine in nineBoard {
+            if !isSingleNum(nine) {
+                return false
+            }
+        }
+        return true
+    }
 }
