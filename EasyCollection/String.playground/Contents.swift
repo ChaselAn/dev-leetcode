7c7ca66e57c4f8d7636f6591771fe5712923e4bc
ancheng <865770853@qq.com>
2019-02-24 14:35:47 +0800

    reverse

--- EasyCollection/String.playground/Contents.swift
+++ EasyCollection/String.playground/Contents.swift
@@ -17,4 +17,37 @@ class Solution {
             (s[i], s[s.count - 1 - i]) = (s[s.count - 1 - i], s[i])
         }
     }
+
+    /*
+     给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+     示例 1:
+     输入: 123
+     输出: 321
+     示例 2:
+     输入: -123
+     输出: -321
+     示例 3:
+     输入: 120
+     输出: 21
+     注意:
+     假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+    */
+    func reverse(_ x: Int) -> Int {
+        let str = "\(x)"
+        var s: [Character] = []
+        var isMinus = false
+        for c in str {
+            if c == "-" {
+                isMinus = true
+                continue
+            }
+            s.append(c)
+        }
+        for i in 0..<s.count / 2 {
+            (s[i], s[s.count - 1 - i]) = (s[s.count - 1 - i], s[i])
+        }
+        let finalStr = String(s)
+        let resInt = Int32(finalStr) ?? 0
+        return Int(isMinus ? -resInt : resInt)
+    }
 }
